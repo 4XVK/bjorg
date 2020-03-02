9bb9e0cd5e9e9bbb51cd30cf5f8cf19c210b131d
4XVK <4XVK@users.noreply.github.com>
2020-03-01 17:52:42 -0800

    fix python pipenv install issue

--- bjorg_core/server_images/debian_10/python.sh
+++ bjorg_core/server_images/debian_10/python.sh
@@ -4,5 +4,7 @@ set +o history
 
 echo "SETUP UP PYTHON NOW OK THANKS"
 
+apt-get install -y python-setuptools
+
 # A crude install of Pipenv
 curl https://raw.githubusercontent.com/pypa/pipenv/master/get-pipenv.py | python
