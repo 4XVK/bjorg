6b787905de3b04572d8cd410781165d80a2db570
4XVK <4XVK@users.noreply.github.com>
2020-03-23 20:31:07 -0700

    nginx optimizing

--- server_core/debian_10/web.sh
+++ server_core/debian_10/web.sh
@@ -3,11 +3,15 @@ HISTSIZE=0
 set +o history
 
 echo "SETUP UP WEB NOW OK THANKS"
+# TODO set rlimits in nginx conf as well as for base system
+# TODO set file paths as variables in head of file?
 
 apt install -y nginx certbot python-certbot-nginx
 
 # Transfer templates
 echo "Transferring templates..."
+mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.bk
+cat templates/nginx.conf > /etc/nginx/nginx.conf
 cat templates/nginx.default > /etc/nginx/sites-available/default
 sed -i 's/server_name _.*/server_name '"$FQDN"';/' /etc/nginx/sites-available/default
 cat templates/index.html > /var/www/html/index.html
