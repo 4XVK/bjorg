1c6d2e2a5cbd604bb3222bebba9c4ecaef89dce3
4XVK <4XVK@users.noreply.github.com>
2020-03-23 21:10:48 -0700

    updated dir org. user issues

--- bjorg/server_core/debian_10/users.sh
+++ bjorg/server_core/debian_10/users.sh
@@ -12,12 +12,12 @@ echo "$HOSTNAME":"$PASSWORD" | chpasswd
 # Make password generically available
 echo "$PASSWORD" > /home/"$HOSTNAME"/bjorg_core/auth_core/"$HOSTNAME"_auth
 
-# Transplant bjorg_core for user
-cp -a /root/bjorg-master/bjorg_core /home/"$HOSTNAME"
+# Transplant bjorg for user
+cp -a /root/bjorg-master/bjorg /home/"$HOSTNAME"
 
 # Transplant sshkey for user
 mkdir /home/"$HOSTNAME"/.ssh
 cp -a /root/.ssh/. /home/"$HOSTNAME"/.ssh/
 
 # Make sure new user is the owner of stuff we transplanted
-chown -R "HOSTNAME" /home/"$HOSTNAME"/
+chown -R "$HOSTNAME" /home/"$HOSTNAME"/
