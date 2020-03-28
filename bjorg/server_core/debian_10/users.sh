abb37ee7d522870f4cd18f223f771993a63bd8d5
4XVK <4XVK@users.noreply.github.com>
2020-03-27 19:10:20 -0700

    auth_core cred transfer fixed fixed permissions

--- bjorg/server_core/debian_10/users.sh
+++ bjorg/server_core/debian_10/users.sh
@@ -9,15 +9,16 @@ adduser "$HOSTNAME" --gecos "" --disabled-password
 usermod -aG sudo "$HOSTNAME"
 echo "$HOSTNAME":"$PASSWORD" | chpasswd
 
-# Make password generically available
-echo "$PASSWORD" > /home/"$HOSTNAME"/bjorg/auth_core/"$HOSTNAME"_auth
-
 # Transplant bjorg for user
 cp -a /root/bjorg-master/bjorg /home/"$HOSTNAME"
+
+# Make password generically available
+echo "$PASSWORD" > /home/"$HOSTNAME"/bjorg/auth_core/"$HOSTNAME"_auth
 
 # Transplant sshkey for user
 mkdir /home/"$HOSTNAME"/.ssh
 cp -a /root/.ssh/. /home/"$HOSTNAME"/.ssh/
 
 # Make sure new user is the owner of stuff we transplanted
-chown -R "$HOSTNAME" /home/"$HOSTNAME"/
+chown -R "$HOSTNAME":"$HOSTNAME" /home/"$HOSTNAME"/bjorg/
+chown -R "$HOSTNAME":"$HOSTNAME" /home/.ssh/
